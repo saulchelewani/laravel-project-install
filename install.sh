c802dd56bb1f3e8bc501c3c81611eaf061a70063
Saul Chelewani <saul@patkay.net>
2019-05-13 07:09:38 +0200

    Added laravel passport support

--- install.sh
+++ install.sh
@@ -10,5 +10,10 @@ composer install
 # generate encription key 
 php artisan key:generate
 
-# migrate the database
-php artisan migrate 
+# install Laravel Passport
+if grep -q 'laravel/passport' 'composer.json'; then
+    php artisan passport:install
+fi
+
+# migrate and seed the database
+php artisan migrate:fresh --seed
