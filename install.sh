a1fc3870aa390be1e4e9524c63f25e0ff114ec72
Saul Chelewani <saul@patkay.net>
2019-05-13 03:28:53 +0200

    Added explanation to the install script

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
+# create .env file
 cp .env.example .env
+
+# open .env to edit env variables 
+vi .env
+
+# install the project with composer
 composer install
+
+# generate encription key 
 php artisan key:generate
+
+# migrate the database
 php artisan migrate 
